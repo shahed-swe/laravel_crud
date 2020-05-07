a06f232204467cba13417bf6ed420d2843455f4c
Shahed Talukder <shahedtalukder51@gmail.com>
2020-05-07 11:54:09 +0600

    hello

--- resources/views/master/master.blade.php
+++ resources/views/master/master.blade.php
@@ -14,14 +14,16 @@
 
 <body>
     <!-- navbar section -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-success">
-        <a class="navbar-brand" href="#">Navbar</a>
+    <nav class="navbar navbar-expand-sm navbar-light bg-success">
+        <a class="navbar-brand" href="#">
+            <img class="d-inline-block align-top" src="{{ resources('img/Logo.png') }}" alt="">
+        </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
+            <ul class="navbar-nav mx-auto">
                 <li class="nav-item active">
                     <a class="nav-link" href="{{ route('home')}}"> Home <span class="sr-only">(current)</span></a>
                 </li>
